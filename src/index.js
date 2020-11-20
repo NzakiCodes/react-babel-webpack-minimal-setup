8edbf9946c7a5da8623e92910941336f23e041fb
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2020-11-20 21:41:42 +0100

    Added React and Babel preset for React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-window.document.write("Hello Webpack......"); 
+import React from 'react';
+import ReactDOM from 'react-dom';
+const App = ()=>(
+    <div>
+        Hello Webpack
+    </div>
+);
+
+ReactDOM.render(<App/>,document.querySelector('.app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
     module: {
         rules: [
             {
-                test: /\.(js)$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
             }
         ]
     },
     resolve: {
-        extensions: ['*', '.js']
+        extensions: ['*', '.js','.jsx']
     },
     output: {
         path: path.resolve(__dirname, './dist'),
